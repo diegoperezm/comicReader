24134b5058cb6de77e64378678439779d1e109c2
diegoperezm <diegoperezcorreo@gmail.com>
2026-07-19 17:36:02 -0400

    add raylib to project: first image working

--- src/main.c
+++ src/main.c
@@ -1,34 +1,39 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include "miniz.h"
-
+#include "raylib.h"
 
 int
 main(int argc, char *argv[]) 
 {
   (void) argc;
   (void) argv;
+// char cwd[1024]; if(getcwd(cwd, sizeof(cwd))) printf("cwd = %s\n", cwd);
+  InitWindow(600, 600, "");
+  SetTargetFPS(60);               
 
+  void* image_data;
+  size_t size;
   mz_zip_archive zip_archive = {0};
-  const char* filename = "../data/data.zip";
-
+  const char* filename = "data/data.zip";
+  size_t image_size; // NOT USED
 // 1. mz_zip_reader_init_file()
   mz_bool status = mz_zip_reader_init_file(&zip_archive,filename,0);
 
   if(!status)
   {
-    fprintf(stderr, "Failed to open zip file: %s\n", filename);
+    mz_zip_error err = mz_zip_get_last_error(&zip_archive);
+    fprintf(stderr,"Failed to open zip file: %s\n",filename);
+    fprintf(stderr,"Error code: %d\n", err);
+    fprintf(stderr, "%s\n", mz_zip_get_error_string(err));
     return EXIT_FAILURE;
   }
 
   printf("Archive opened successfully.\n");
 
 // 2. mz_zip_reader_get_num_files()
-
   mz_uint file_count = mz_zip_reader_get_num_files(&zip_archive);
   printf("Files: %u\n\n", file_count);
-
-  void* data;
-  size_t size;
 
   for(mz_uint i=0; i < file_count; i++)
   {
@@ -36,8 +41,8 @@ main(int argc, char *argv[])
     mz_zip_archive_file_stat  stat; 
     if(!mz_zip_reader_file_stat(&zip_archive,i,&stat))
     {
-	printf("Cannot read file %u\n",i);
-	continue;
+    	printf("Cannot read file %u\n",i);
+     	continue;
     }
     printf("Index:    %u\n", stat.m_file_index);
     printf("Filename: %s\n",stat.m_filename);
@@ -48,24 +53,66 @@ main(int argc, char *argv[])
     printf("Directory?: %s\n",
 	   stat.m_is_directory ? "yes" : "no");
     printf("\n"); 
-    data =   mz_zip_reader_extract_to_heap(
-		    &zip_archive,
-		    stat.m_file_index,
-	            &size,
-		    0
-		    );
-  }
-
-
-  
 
 // 4. mz_zip_reader_extract_to_mem() 
-// 4. mz_zip_reader_extract_to_heap() 
+// 4. or mz_zip_reader_extract_to_heap() 
+   image_data =  mz_zip_reader_extract_to_heap( 
+                &zip_archive,
+        		    0,
+	              &size,
+		            0);
+  }
 
+  if (image_data == NULL)
+  {
+    fprintf(stderr, "Extraction failed: %s\n",
+            mz_zip_get_error_string(mz_zip_get_last_error(&zip_archive)));
+    return EXIT_FAILURE;
+  }
+   Image image = LoadImageFromMemory(
+    ".png",
+    image_data,
+    (int)size
+   );
 
+   if (image.data == NULL)
+   {
+    fprintf(stderr, "Failed to decode image.\n");
+    mz_free(image_data);
+    return EXIT_FAILURE;
+   }
+
+   Texture2D texture = LoadTextureFromImage(image);
+   mz_free(image_data);
+   UnloadImage(image);
+
+   while (!WindowShouldClose())    
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+          ClearBackground(RAYWHITE);
+          DrawTexture(texture,0,0, WHITE);
+          DrawText("this IS a texture!", 360, 370, 10, GRAY);
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+  UnloadTexture(texture);
+  CloseWindow();                
 
 // 5. mz_zip_reader_end()
   mz_zip_reader_end(&zip_archive);
-
   return EXIT_SUCCESS;
 }
+
+
+
+
+
+
