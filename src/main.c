f5a5b4af23ca80874491346032c1c4687047f908
diegoperezm <diegoperezcorreo@gmail.com>
2026-07-20 18:56:42 -0400

    cleaning

--- src/main.c
+++ src/main.c
@@ -17,7 +17,6 @@ main(int argc, char *argv[])
   mz_zip_archive zip_archive = {0};
   const char* filename = "data/data.zip";
  
-// 1. mz_zip_reader_init_file()
   mz_bool status = mz_zip_reader_init_file(&zip_archive,filename,0);
 
   if(!status)
@@ -31,13 +30,11 @@ main(int argc, char *argv[])
 
   printf("Archive opened successfully.\n");
 
-// 2. mz_zip_reader_get_num_files()
   mz_uint file_count = mz_zip_reader_get_num_files(&zip_archive);
   printf("Files: %u\n\n", file_count);
 
   for(mz_uint i=0; i < file_count; i++)
   {
-// 3. mz_zip_reader_file_stat()
     mz_zip_archive_file_stat  stat; 
     if(!mz_zip_reader_file_stat(&zip_archive,i,&stat))
     {
@@ -54,13 +51,12 @@ main(int argc, char *argv[])
 	   stat.m_is_directory ? "yes" : "no");
     printf("\n"); 
 
-// 4. mz_zip_reader_extract_to_mem() 
-// 4. or mz_zip_reader_extract_to_heap() 
+// 4. mz_zip_reader_extract_to_mem() or mz_zip_reader_extract_to_heap() 
    image_data =  mz_zip_reader_extract_to_heap( 
-                &zip_archive,
-        		    0,
-	              &image_size,
-		            0);
+                  &zip_archive,
+        	  0,
+                  &image_size,
+		  0);
   }
 
   if (image_data == NULL)
